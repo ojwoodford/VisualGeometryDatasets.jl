4f66855555f54a77dcee0e7e2ea26b64b2c761cf
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2022-08-01 14:06:57 +0100

    Fix test errors (#4)
    * Update runtests.jl
    
    * Create test per dataset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using VGDatasets
 using Test
-import UserConfig
+import UserConfig.localstorestring
 
 @testset "VGDatasets.jl" begin
 # Create a temporary data folder and store the path to it
@@ -11,21 +11,23 @@ if isempty(vgdir)
 end
 localstorestring("VGDatasets data", dname)
 
-# Download a dataset and check some values
-data = loadbaldataset("problem-16-22106")
-@test size(data.measurementindices) == (2, 83718)
-@test vec(data.measurementindices[:,[1 end]]) == [1, 1, 16, 22106]
-@test size(data.measurements) == (2, 83718)
-@test typeof(data.measurements[1]) == Float64
-@test typeof(data.cameras[1]) == Float64
-@test typeof(data.landmarks[1]) == Float64
-@test data.measurements[1,end] == -196.32
+@testset "BAL datasets" begin
+    # Download a dataset and check some values
+    data = loadbaldataset("problem-16-22106")
+    @test size(data.measurementindices) == (2, 83718)
+    @test vec(data.measurementindices[:,[1 end]]) == [1, 1, 16, 22106]
+    @test size(data.measurements) == (2, 83718)
+    @test typeof(data.measurements[1]) == Float64
+    @test typeof(data.cameras[1]) == Float64
+    @test typeof(data.landmarks[1]) == Float64
+    @test data.measurements[1,end] == -196.32
 
-# Check the storage type can be specified
-data = loadbaldataset("problem-16-22106", Float32)
-@test typeof(data.measurements[1]) == Float32
-@test typeof(data.cameras[1]) == Float32
-@test typeof(data.landmarks[1]) == Float32
+    # Check the storage type can be specified
+    data = loadbaldataset("problem-16-22106", Float32)
+    @test typeof(data.measurements[1]) == Float32
+    @test typeof(data.cameras[1]) == Float32
+    @test typeof(data.landmarks[1]) == Float32
+end
 
 # Revert the database path and delete the temp directory
 localstorestring("VGDatasets data", vgdir)
